e16d34373e645d13ad1fc3572705dd56fecc5c03
Jaap Vossers <jaap.vossers@newday.co.uk>
2022-12-06 06:37:31 +0000

    day 6 part 1

--- Day6TuningTrouble/Day6TuningTrouble/Tests.cs
+++ Day6TuningTrouble/Day6TuningTrouble/Tests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Text;
+using FluentAssertions;
 using Xunit;
 
 namespace Day6TuningTrouble
@@ -10,5 +11,57 @@ namespace Day6TuningTrouble
         {
             File.ReadAllLines("input.txt").Length.Should().BeGreaterThan(0);
         }
+
+        [Fact]
+        public void FindSolutionPart1()
+        {
+            var sut = new Scanner();
+
+            int position = sut.Scan(File.ReadAllLines("input.txt").First());
+
+            Console.WriteLine(position);
+        }
+
+        [Theory]
+        [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        public void ShouldFindMarkerPosition(string subject, int expectedPosition)
+        {
+            var sut = new Scanner();
+
+            int position = sut.Scan(subject);
+
+            position.Should().Be(expectedPosition);
+        }
     }
+
+    public class Scanner
+    {
+        public int Scan(string subject)
+        {
+            int counter = 0;
+            var buffer = new List<char>();
+            
+            foreach (var c in subject)
+            {
+                buffer.Add(c);
+                counter++;
+
+                if (buffer.Distinct().Count() < buffer.Count)
+                {
+                    buffer.RemoveAt(0);
+                }
+
+                if (buffer.Count > 3)
+                {
+                    return counter;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+
 }

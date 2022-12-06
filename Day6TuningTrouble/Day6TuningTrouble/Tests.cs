83e8a21b27f2da2ed233be9e22765e84e71624f2
Jaap Vossers <jaap.vossers@newday.co.uk>
2022-12-06 06:08:27 +0000

    day 6 setup

---
+++ Day6TuningTrouble/Day6TuningTrouble/Tests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Day6TuningTrouble
+{
+    public class Tests
+    {
+        [Fact]
+        public void ShouldReadLines()
+        {
+            File.ReadAllLines("input.txt").Length.Should().BeGreaterThan(0);
+        }
+    }
+}

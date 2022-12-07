66f2b683e8c8f6085268ff8845a767a4a1b27a58
Jaap Vossers <jaap.vossers@newday.co.uk>
2022-12-07 05:43:46 +0000

    day 7 setup

---
+++ Day7NoSpaceLeftOnDevice/Day7NoSpaceLeftOnDevice/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
+using FluentAssertions;
+using Xunit;
+
+namespace Day7NoSpaceLeftOnDevice
+{
+    public class Files
+    {
+        public const string InputFile = "input.txt";
+        public const string InputFileTest = "input.test.txt";
+    }
+
+    public class Tests
+    {
+        [Theory]
+        [InlineData(Files.InputFile)]
+        [InlineData(Files.InputFileTest)]
+        public void ShouldReadFile(string fileName)
+        {
+            File.ReadAllLines(fileName).Length.Should().BeGreaterThan(0);
+        }
+
+        // build filestructure hierarchy
+        // calc direct folder size
+        // add recursion to include child folder sizes in calculation
+        // filter where dir.size < 100000
+        // sum of the above list of filtered list
+    }
+}

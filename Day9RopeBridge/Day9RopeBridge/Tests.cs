6e2b0daf29b945b4c817d9aec093a107a60641e9
Jaap Vossers <jaap.vossers@newday.co.uk>
2022-12-09 06:39:28 +0000

    day 9 set up

---
+++ Day9RopeBridge/Day9RopeBridge/Tests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Day9RopeBridge
+{
+    public class Tests
+    {
+        [Fact]
+        public void ShouldReadFiles()
+        {
+            File.ReadAllLines("input.txt").Should().NotBeEmpty();
+        }
+    }
+}

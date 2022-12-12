232d49197f7c376855fdbc8456287fef6b804af9
Jaap Vossers <jaap.vossers@newday.co.uk>
2022-12-12 01:28:23 +0000

    day 11 setup

---
+++ Day11MonkeyInTheMiddle/Day11MonkeyInTheMiddle/Tests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Day11MonkeyInTheMiddle
+{
+    public class Tests
+    {
+        [Fact]
+        public void ShouldReadLines()
+        {
+            File.ReadAllLines("input.txt").Should().NotBeEmpty();
+        }
+    }
+}

2bae7d2fb19bf55622a33b605f481b929c594b00
Jaap Vossers <jaap.vossers@newday.co.uk>
2023-12-04 09:46:01 +0000

    day 4 part 2

--- 2023/Day4Scratchcards/Part2/Part2.cs
+++ 2023/Day4Scratchcards/Part2/Part2.cs
@@ -1,17 +1,58 @@
 ﻿using FluentAssertions;
-using Xunit.Abstractions;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Part2
 {
     public class Line
     {
         private readonly string _text;
+        
+        public HashSet<int> WinningNumbersSet { get; set; }
+        public List<Number> MyNumbers { get; set; }
+        public List<Number> WinningNumbers { get; set; }
+        public List<int> MyWinningNumbers { get; set; }
+
+        public int CardNumber { get; set; }
+
+        public int GetPoints()
+        {
+            int points = (int)Math.Pow((double)2, (double)MyWinningNumbers.Count-1);
+
+            return points;
+        }
 
         public Line(string text)
         {
             _text = text;
+            ParseNumbers();
         }
+
+        private void ParseNumbers()
+        {
+            var colonSplit = _text.Replace("  ", " ").Split(':');
+            CardNumber = int.Parse(colonSplit[0].Replace("Card ", ""));
+
+            var numberSets = colonSplit[1].Split('|');
+            WinningNumbers = numberSets[0].Trim().Split(' ').Select(n => new Number(int.Parse(n))).ToList();
+            WinningNumbersSet = new HashSet<int>(WinningNumbers.Select(n => n.Value));
+            
+            MyNumbers = numberSets[1].Trim().Split(' ').Select(n => new Number(int.Parse(n))).ToList();
+            MyWinningNumbers = MyNumbers
+                .Where(n => WinningNumbersSet.Contains(n.Value))
+                .Select(n => n.Value)
+                .ToList();
+        }
+    }
+
+    public class Number
+    {
+        public Number(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; set; }
     }
 
     public class Solution
@@ -23,9 +64,35 @@ namespace Part2
             _lines = lines;
         }
 
-        public string Solve()
+        public int Solve(ITestOutputHelper output)
         {
-            return string.Empty;
+            int count = 0;
+
+            Queue<Line> queue = new Queue<Line>();
+            
+            foreach (var line in _lines)
+            {
+                queue.Enqueue(line);
+                count++;
+                output.WriteLine(count.ToString());
+            }
+
+            while (queue.Count > 0)
+            {
+                var line = queue.Dequeue();
+
+                for (int i = 0; i < line.MyWinningNumbers.Count; i++)
+                {
+                    if (line.CardNumber + i < _lines.Count)
+                    {
+                        queue.Enqueue(_lines[line.CardNumber+i]);
+                        count++;
+                        output.WriteLine(count.ToString());
+                    }
+                }
+            }
+
+            return count;
         }
     }
 
@@ -41,7 +108,18 @@ namespace Part2
         [Fact]
         public void Test()
         {
-            
+            var solution = new Solution(
+                new List<Line>()
+                {
+                    new Line("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"),
+                    new Line("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"),
+                    new Line("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"),
+                    new Line("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"),
+                    new Line("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"),
+                    new Line("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"),
+                });
+
+            solution.Solve(_output).Should().Be(30);
         }
 
         [Fact]
@@ -55,7 +133,7 @@ namespace Part2
         {
             var solution = new Solution(File.ReadAllLines("input.txt").Select(line => new Line(line)).ToList());
 
-            _output.WriteLine(solution.Solve());
+            _output.WriteLine("Answer: " + solution.Solve(_output).ToString()); // 11024379
         }
     }
 }
